0f9bc8834a4e976a718b98a7fbb8319df0d99a56
Rafiq Shahsawary <rshahsawary1@hotmail.com>
2016-12-13 23:27:35 +0000

    broke index into section to include

--- controllers/contactController.js
+++ controllers/contactController.js
@@ -2,10 +2,14 @@ const Contact = require("../models/contact");
 const mailer = require("../lib/email");
 
 class ContactController {
-    static sendContactRequest(req, res){
-        try {
-            const contactRequest = new Contact(req.body);
-            mailer.sendEmail(contactRequest)
+    static speak(req, res){         // the get request from routes/contact invokes this function
+        res.render("hello.ejs");    //  will look in ../views for 'hello.ejs' to display in the browser at the route given in ../routes/contact    
+    }
+    static sendContactRequest(req, res){            // POST requests are handled by models
+        try {                                       // req.body is a JSON object (contact form stuff) send to Contact in ../models/contact
+            const contactRequest = new Contact(req.body);   // Contact extracts the information of the object 
+            // The object is passed to sendEmail()
+            mailer.sendEmail(contactRequest)         
                 .then(() => {
                     console.log("It worked!");
                     res.redirect("/");
--- routes/contact.js
+++ routes/contact.js
@@ -3,5 +3,5 @@ const router = express.Router();
 const contactController = require("../controllers/contactController");
 
 router.post("/contact", contactController.sendContactRequest);
-
+router.get("/contact", contactController.speak);
 module.exports = router;
