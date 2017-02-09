fc8babc9eb46c7d167d6ae2e60eaeaad2cdf0d8f
Rafiq Shahsawary <rshahsawary1@hotmail.com>
2017-02-09 12:35:33 +0000

    changing links

--- public/js/script.js
+++ public/js/script.js
@@ -15,26 +15,28 @@ $(document).ready(function() {
     let fullMergedArray = $.merge(mergedArray, scrollSpeedArray);
     // effectively loop through scrollArray and access higher positions (scrollTo and scrollSpeed arrays) using j+6 and j+12
     for(let j=0; j<6; j++) {
-        $(fullMergedArray[j]).click(function(){
+        $(fullMergedArray[j]).click(() => {
             $("html, body").animate({
                 scrollTop: $(fullMergedArray[j+6]).trigger("click").offset().top
             }, fullMergedArray[j+12]);
         });
     }
+
+
     // perform same process for side nav
     let sideScrollArray = ["#jobsSide", "#educationSide", "#contactSide", "#skillsSide", "#aboutSide", "#projectsSide"];
-    let sideScrollToArray = ["#collapseJobs", "#collapseEducation", "#contactFooter", "#about", "#about", "#projects"];
+    let sideScrollToArray = ["#collapseJobs", "#collapseEducation", "#contactFooter", "#about", "#about", "#projectRow"];
     let sideMergedArray = $.merge(sideScrollArray, sideScrollToArray);
     let sideScrollSpeedArray = [900, 1100, 1300, 700, 700, 1400];
     let sideFullMergedArray = $.merge(sideMergedArray, sideScrollSpeedArray);
     for(let j=0; j<6; j++) {
-        $(sideFullMergedArray[j]).click(function(){
+        $(sideFullMergedArray[j]).click(() => {
             $("html, body").animate({
                 scrollTop: $(sideFullMergedArray[j+6]).trigger("click").offset().top
             }, sideFullMergedArray[j+12]);
-        });
+            $('.side-nav').sideNav('hide');
+        }); 
     }
-
 
     return false;
 });
